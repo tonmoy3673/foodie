afcd1255cff317bebe277f623c72eccb2af79174
tonmoy <tonmoy3673@gmail.com>
2024-05-25 22:50:03 +0600

    :bulb: set routes

---
+++ src/components/Router/router.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "../Layout/Layout";
+
+export const router=createBrowserRouter([
+    {
+        path:'/',
+        element:<Layout/>,
+        children:[
+            {
+                path:'/',
+               
+            }
+        ]
+    }
+])
