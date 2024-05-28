53e1c6378d71a1d7c41d0c9386466cd90b8f04e9
tonmoy <tonmoy3673@gmail.com>
2024-05-28 13:38:08 +0600

    :sparkles: implemented add product feature

--- src/components/Pages/Dashboard/AddProducts.jsx
+++ src/components/Pages/Dashboard/AddProducts.jsx
@@ -1,7 +1,34 @@
+import toast from "react-hot-toast";
 import { FaLocationArrow } from "react-icons/fa6";
 
 
 const AddProducts = () => {
+  const handleSubmit=async(e)=>{
+  e.preventDefault();
+  const form=e.target;
+  const id=form.id.value;
+  const title=form.name.value;
+  const price=form.price.value;
+  const brand=form.brand.value;
+  const description=form.description.value;
+  const image_url=form.photo.value;
+ const data={title,brand,description,image_url, id,price};
+ console.log(data);
+    await fetch("http://localhost:3000/foods",{
+      method:"POST",
+      headers:{
+        "Content-type":"application/json"
+      },
+      body:JSON.stringify(data)
+      
+    })
+    .then((res)=>res.json())
+    .then((data)=>console.log("fetch data :",data))
+    form.reset();
+    toast.success('New Food Added Successfully!!')
+    
+
+  };
     return (
         <div>
             <h2 className="text-center pb-4 md:pb-8 text-xl lg:text-3xl text-[#880769] font-semibold">Add New Food Items</h2>
@@ -10,7 +37,7 @@ const AddProducts = () => {
                 {/* =========== Form Part ======= */}
             <div className="rounded-lg items-center py-7 mx-auto">
               
-              <form action="#" className=" ">
+              <form onSubmit={handleSubmit} className=" ">
                 <div className="text-base">
                   <div className="grid grid-cols-1 gap-6 md:gap-12 md:grid-cols-2 lg:mb-8 mb-4">
                     <div>
@@ -52,7 +79,7 @@ const AddProducts = () => {
                       className="w-full rounded-lg bg-[#C477B126] shadow-sm shadow-black p-3 lg:pr-16 pr-3  text-sm"
                       placeholder="Type Brand Name "
                       type="text"
-                      id="brands"
+                      id="brand"
                     />
                   </div>
 
@@ -86,7 +113,7 @@ const AddProducts = () => {
                 </div>
                 <div>
                     <h5 className="mb-2 text-[#880769] text-base">ID *</h5>
-                    <label className="sr-only" htmlFor="brands">
+                    <label className="sr-only" htmlFor="id">
                       ID
                     </label>
                     <input
@@ -100,6 +127,7 @@ const AddProducts = () => {
 
                 <div className=" flex md:block xs:justify-center xs:items-center xs:mx-auto">
                   <button
+                  value='submit'
                     type="submit"
                     className="rounded-md bg-[#880769] hover:bg-[#FBCC21E4] px-10 py-4 md:px-20  font-semibold text-base  text-white items-center flex lg:mt-13 mt-8 "
                   >
